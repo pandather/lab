a57fdcb399a8a74b236b122f3a4e4eeb504c5c79
Shrey Desai <shreydesai@me.com>
2016-12-13 00:37:18 -0600

    updated scraper

--- lab/scraper.py
+++ lab/scraper.py
@@ -1,6 +1,21 @@
 from bs4 import BeautifulSoup, NavigableString
 from urllib.request import urlopen
 
+class Host:
+
+    ALLOWED_KEYS = [
+        'name',
+        'status',
+        'uptime',
+        'users',
+        'load'
+    ]
+
+    def __init__(self, **kwargs):
+        for k in kwargs:
+            if k in self.ALLOWED_KEYS:
+                setattr(self, k, kwargs.get(k))
+
 class Scraper:
 
     PARSER = 'html.parser'
@@ -28,19 +43,28 @@ class Scraper:
                     data.append(text)
 
             kwargs['name'] = data[0]
+
             status = data[1]
             if status == 'up':
                 kwargs['status'] = True
             elif status == 'down':
                 kwargs['status'] = False
-            kwargs['uptime'] = data[2]
-            kwargs['users'] = int(data[3])
-            kwargs['load'] = float(data[4])
 
-            host = Host(kwargs)
+            kwargs['uptime'] = data[2]
+
+            users = data[3]
+            if len(users) == 0:
+                kwargs['users'] = 0
+            else:
+                kwargs['users'] = int(data[3])
+
+            load = data[4]
+            if len(load) == 0:
+                kwargs['load'] = 0
+            else:
+                kwargs['users'] = float(data[4])
+
+            host = Host(**kwargs)
             hosts.append(host)
 
         return hosts
-
-s = Scraper('http://apps.cs.utexas.edu/unixlabstatus/')
-s.parse_html()
