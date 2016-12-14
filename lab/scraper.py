5057f41be4b4f12b8f349a1ed1aeaea346b0782e
Shrey Desai <shreydesai@me.com>
2016-12-13 23:09:57 -0600

    updated Host class

--- lab/scraper.py
+++ lab/scraper.py
@@ -11,64 +11,80 @@ class Host:
         'load'
     ]
 
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs: dict) -> None:
         for k in kwargs:
             if k not in self.ALLOWED_KEYS:
                 raise AttributeError
 
         self.name = kwargs.get('name')
+        self.status = self.__status(kwargs.get('status'))
+        self.uptime = self.__uptime(kwargs.get('uptime'))
+        self.users = self.__users(kwargs.get('users'))
+        self.load = self.__load(kwargs.get('load'))
 
-        self.status = kwargs.get('status')
-        if self.status == 'up':
-            self.status = True
-        elif self.status == 'down':
-            self.status = False
+    def __status(self, status):
+        if status == 'up':
+            return True
+        if status == 'down':
+            return False
 
-        self.uptime = kwargs.get('uptime')
-
-        self.users = kwargs.get('users')
-        if len(self.users) == 0:
-            self.users = 0
+    def __uptime(self, uptime):
+        if not '+' in uptime:
+            return 0
         else:
-            self.users = int(self.users)
+            hour = uptime.split('+')[0]
+            return int(hour)
 
-        self.load = kwargs.get('load')
-        if len(self.load) == 0:
-            self.load = 0.0
+    def __users(self, users):
+        if len(users) == 0:
+            return 0
         else:
-            self.load = float(self.load)
+            return int(users)
+
+    def __load(self, load):
+        if len(load) == 0:
+            return 0.0
+        else:
+            return float(load)
 
 class Scraper:
 
     PARSER = 'html.parser'
 
-    def __init__(self, url: str):
+    def __init__(self, url):
         self.url = url
+        self.hosts = []
+        self.timestamp = ''
 
-    def get_soup(self):
+    def __get_soup(self):
         html = urlopen(self.url)
         soup = BeautifulSoup(html, self.PARSER)
         return soup
 
-    def parse_html(self):
-        soup = self.get_soup()
-        hosts = []
-
+    def __parse_html(self):
+        soup = self.__get_soup()
         rows = soup.find_all('tr')
+
+        for _ in rows[0].children:
+            time = _.get_text()
+            self.timestamp = time
+
         for i in range(3, len(rows)):
             data = []
-            kwargs = {}
-
             for row in rows[i].children:
                 if type(row) != NavigableString:
                     text = row.get_text()
                     data.append(text)
 
-            for i in range(len(data)):
-                key = Host.ALLOWED_KEYS[i]
-                kwargs[key] = data[i]
+            host = Host(name=data[0], status=data[1], uptime=data[2],
+                users=data[3], load=data[4])
+            self.hosts.append(host)
 
-            host = Host(**kwargs)
-            hosts.append(host)
+    def get_hosts(self):
+        return self.hosts
 
-        return hosts
+    def get_timestamp(self):
+        return self.timestamp
+
+    def update(self):
+        self.__parse_html()
