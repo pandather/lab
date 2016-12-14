a5fff0a7477063e65542d3f46db830c700377225
Shrey Desai <shreydesai@me.com>
2016-12-13 23:10:07 -0600

    added/edited API endpoints

--- lab/server.py
+++ lab/server.py
@@ -6,69 +6,135 @@ URL = 'http://apps.cs.utexas.edu/unixlabstatus/'
 app = Flask(__name__)
 scraper = Scraper(URL)
 
-@app.route('/hosts/all', methods=['GET'])
+@app.route('/all', methods=['GET'])
 def get_all_hosts():
-    """List all UNIX hosts"""
     data = []
-    for host in scraper.parse_html():
+    scraper.update()
+    timestamp = scraper.get_timestamp()
+    for host in scraper.get_hosts():
         host_vars = host.__dict__
         data.append(host_vars)
     return jsonify({
         'status': True,
+        'timestamp': timestamp,
         'data': data
     })
 
-@app.route('/hosts/running', methods=['GET'])
-def get_running_hosts():
-    """List UNIX hosts whose status is up"""
+@app.route('/up', methods=['GET'])
+def get_up_hosts():
     data = []
-    for host in scraper.parse_html():
+    scraper.update()
+    timestamp = scraper.get_timestamp()
+    for host in scraper.get_hosts():
         host_vars = host.__dict__
         if host_vars['status']:
-            name = host_vars['name']
-            data.append(name)
+            data.append(host_vars)
     return jsonify({
         'status': True,
+        'timestamp': timestamp,
         'data': data
     })
 
-@app.route('/hosts/<int:users>', methods=['GET'])
-def get_hosts_with_users(users):
-    """List UNIX hosts with users less than the number of
-    users specified"""
+@app.route('/down', methods=['GET'])
+def get_down_hosts():
     data = []
-    for host in scraper.parse_html():
+    scraper.update()
+    timestamp = scraper.get_timestamp()
+    for host in scraper.get_hosts():
         host_vars = host.__dict__
-        if host_vars['users'] < users:
-            name = host_vars['name']
-            data.append(name)
+        if not host_vars['status']:
+            data.append(host_vars)
     return jsonify({
         'status': True,
+        'timestamp': timestamp,
         'data': data
     })
 
-@app.route('/hosts/<float:load>', methods=['GET'])
-def get_hosts_with_load(load):
-    """List UNIX hosts with load less than the load
-    number specified"""
+@app.route('/users', methods=['GET'])
+def get_hosts_by_users():
     data = []
-    for host in scraper.parse_html():
+    req = request.args
+    up, threshold = bool(req['up']), int(req['threshold'])
+    scraper.update()
+    timestamp = scraper.get_timestamp()
+    for host in scraper.get_hosts():
         host_vars = host.__dict__
-        if host_vars['load'] < load:
-            name = host_vars['name']
-            data.append(name)
+        if up and host_vars['users'] <= threshold or \
+        not up and host_vars['users'] >= threshold:
+            data.append(host_vars)
     return jsonify({
         'status': True,
+        'timestamp': timestamp,
         'data': data
     })
 
-#@app.route('/hosts/<str:name>', methods=['GET'])
-#def get_specific_host(name):
-#    pass
+@app.route('/load', methods=['GET'])
+def get_hosts_by_load():
+    data = []
+    req = request.args
+    up, threshold = bool(req['up']), float(req['threshold'])
+    scraper.update()
+    timestamp = scraper.get_timestamp()
+    for host in scraper.get_hosts():
+        host_vars = host.__dict__
+        if up and host_vars['load'] <= threshold or \
+        not up and host_vars['load'] >= threshold:
+            data.append(host_vars)
+    return jsonify({
+        'status': True,
+        'timestamp': timestamp,
+        'data': data
+    })
 
-@app.route('/hosts/sorted', methods=['GET'])
-def get_sorted_hosts(name):
-    pass
+@app.route('/uptime', methods=['GET'])
+def get_hosts_by_uptime():
+    data = []
+    req = request.args
+    up, threshold = bool(req['up']), int(req['threshold'])
+    scraper.update()
+    timestamp = scraper.get_timestamp()
+    for host in scraper.get_hosts():
+        host_vars = host.__dict__
+        if up and host_vars['uptime'] <= threshold or \
+        not up and host_vars['uptime'] >= threshold:
+            data.append(host_vars)
+    return jsonify({
+        'status': True,
+        'timestamp': timestamp,
+        'data': data
+    })
+
+@app.route('/host', methods=['GET'])
+def get_host_by_name():
+    data = []
+    req = request.args
+    name = req['name']
+    scraper.update()
+    timestamp = scraper.get_timestamp()
+    for host in scraper.get_hosts():
+        host_vars = host.__dict__
+        if name == host_vars['name']:
+            data.append(host_vars)
+            break
+    return jsonify({
+        'status': True,
+        'timestamp': timestamp,
+        'data': data
+    })
+
+@app.route('/sorted', methods=['GET'])
+def get_sorted_hosts():
+    scraper.update()
+    timestamp = scraper.get_timestamp()
+    hosts = sorted(scraper.get_hosts(),
+        key=lambda x: (x.status, x.users, x.load)
+    )
+    data = [host.__dict__ for host in hosts]
+    return jsonify({
+        'status': True,
+        'timestamp': timestamp,
+        'data': data
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
